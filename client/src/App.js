7354211c173b46de8376d42353b73db0869f8cfb
heartyhardy <charith.n.rathnayake@gmail.com>
2021-07-28 22:15:48 +0530

    FrontEnd MusicListItem comp added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import MusicListItem from "./components/music/music_list_item/MusicListItem";
 
 function App() {
   const [data, setData] = useState(null);
@@ -13,9 +15,14 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
+      <div className="temp"><p>{!data ? "Loading..." : data}</p></div>
+      <MusicListItem playing={false} song={"TSFH - Love and Loss"}/>
+      <MusicListItem playing={true} song={"TSFH  -  Victory"}/>
+      <MusicListItem playing={false} song={"TSFH - Atlantis"}/>
+      <MusicListItem playing={false} song={"TSFH - Impossible"}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
